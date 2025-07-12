07054d34e4d774b7373aaa289d8a1782194f32a1
CodebarPhantom <eryangamer10@gmail.com>
2025-07-12 21:18:28 +0700

    [refactor] congregation_id to location_id

--- database/migrations/2024_10_07_211201_add_congregation_id_in_users.php
+++ database/migrations/2024_10_07_211201_add_congregation_id_in_users.php
@@ -12,7 +12,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::table('users', function (Blueprint $table) {
-            $table->unsignedBigInteger('congregation_id')->nullable()->after('id');
+            $table->unsignedBigInteger('location_id')->nullable()->after('id');
             $table->string('url_image')->nullable()->after('name');
 
         });
